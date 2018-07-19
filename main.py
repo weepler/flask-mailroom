192d128d4ee00210f508c59e239635bbaa5fc22e
Wayne Olson <weepler@Wayne-Olsons-MacBook-Pro.local>
2018-07-18 18:23:16 -0700

    pushing jinja and main templates

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import base64
 
 from flask import Flask, render_template, request, redirect, url_for, session
 
-from model import Donation 
+from model import *
 
 app = Flask(__name__)
 
@@ -15,7 +15,23 @@ def home():
 def all():
     donations = Donation.select()
     return render_template('donations.jinja2', donations=donations)
-    
+
+@app.route('/create/', methods=['GET', 'POST'])
+def create():
+    if request.method == 'POST':
+
+        name = request.form['name']
+        donation_amount = request.form['value']
+
+        donor = Donor.get(name=request.form['name'])
+        donor.save()
+
+        donation = Donation(donor=donor,value=int(request.form['value']))
+        donation.save()
+        return redirect(url_for('all'))
+
+    else:
+        return render_template('create.jinja2')
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
